fb75f3980b0dc241b1a2df2418f01a97ee385c3e
ysb000 <yusubeen0517@gmail.com>
2026-07-27 13:18:26 +0900

    연습

---
+++ 04-kfc/index.js
@@ -0,0 +1,5 @@
+// 변수만들기
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const dynamicImg = document.querySelector('#big');
+console.log(prevBtn, nextBtn, dynamicImg);
